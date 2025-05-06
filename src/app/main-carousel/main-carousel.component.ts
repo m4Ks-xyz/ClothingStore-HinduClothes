90ccfbd9f86ebd9b93bf89851f2f649b7c15a13b
Maks <maksymiliantomowiak321@gmail.com>
2025-05-06 14:54:37 +0200

    feat: restructure app layout

---
+++ src/app/main-carousel/main-carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-main-carousel',
+	imports: [],
+	templateUrl: './main-carousel.component.html',
+	styleUrl: './main-carousel.component.scss',
+})
+export class MainCarouselComponent {}
