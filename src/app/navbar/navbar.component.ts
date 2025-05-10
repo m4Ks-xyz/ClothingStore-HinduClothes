380afb761cefa2045e2a780cb184d4875fc3ee9d
Maks <maksymiliantomowiak321@gmail.com>
2025-05-11 00:42:47 +0200

    feat: navbar md + lg+

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
 	selector: 'app-navbar',
-	imports: [MatSidenavModule, MatButtonModule, MatIconModule],
+	imports: [MatSidenavModule, MatButtonModule, MatIconModule, MatMenuModule],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
