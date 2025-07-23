42664db681630929b9dd91bd2576c10975a7851d
Maks <maksymiliantomowiak321@gmail.com>
2025-07-23 21:07:42 +0200

    feat: devOps .env

---
+++ src/.env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+	readonly NG_APP_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
--- src/app/auth/data-acces/config/api.ts
+++ src/app/auth/data-acces/config/api.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = 'http://maluch.mikr.us:40602';
+export const BASE_API_URL = '';
 export const TOKEN_STORAGE_KEY = 'token';
